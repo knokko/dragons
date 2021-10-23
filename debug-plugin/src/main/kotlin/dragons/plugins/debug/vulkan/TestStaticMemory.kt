61065861967e325a413d0b3f06719a3c05cbb068
knokko <knokogator@hotmail.com>
2021-10-23 19:14:08 +0200

    Finish the semi-automatic static memory test for now

--- debug-plugin/src/main/kotlin/dragons/plugins/debug/vulkan/TestStaticMemory.kt
+++ debug-plugin/src/main/kotlin/dragons/plugins/debug/vulkan/TestStaticMemory.kt
@@ -3,16 +3,20 @@ package dragons.plugins.debug.vulkan
 import dragons.plugin.PluginInstance
 import dragons.plugin.interfaces.vulkan.VulkanStaticMemoryUser
 import dragons.vulkan.memory.VulkanBufferRange
+import dragons.vulkan.memory.VulkanImage
 import dragons.vulkan.memory.claim.BufferMemoryClaim
+import dragons.vulkan.memory.claim.ImageMemoryClaim
+import dragons.vulkan.memory.claim.StagingBufferMemoryClaim
 import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.launch
 import org.lwjgl.system.MemoryUtil.memAddress
 import org.lwjgl.vulkan.VK12.*
+import java.nio.ByteBuffer
 
 class TestStaticMemory: VulkanStaticMemoryUser {
     override fun claimStaticMemory(pluginInstance: PluginInstance, agent: VulkanStaticMemoryUser.Agent) {
         if (pluginInstance.gameInitProps.mainParameters.testParameters.staticMemory) {
             for (queueFamily in agent.queueManager.allQueueFamilies) {
-                // TODO Await this
                 val prefilledIndirectResult = CompletableDeferred<VulkanBufferRange>()
                 agent.claims.buffers.add(
                     BufferMemoryClaim(
@@ -22,12 +26,152 @@ class TestStaticMemory: VulkanStaticMemoryUser {
                         queueFamily = queueFamily,
                         storeResult = prefilledIndirectResult
                     ) { byteBuffer ->
-                        println("Prefilled indirect buffer start is ${memAddress(byteBuffer)}")
-                        assert(byteBuffer.capacity() == 200)
+                        println("Prefilled indirect buffer starts at ${memAddress(byteBuffer)}")
+                        check(byteBuffer.capacity() == 200)
                     }
                 )
 
                 val prefilledStorageResult = CompletableDeferred<VulkanBufferRange>()
+                agent.claims.buffers.add(
+                    BufferMemoryClaim(
+                        size = 300,
+                        usageFlags = VK_BUFFER_USAGE_STORAGE_BUFFER_BIT,
+                        dstAccessMask = VK_ACCESS_SHADER_READ_BIT or VK_ACCESS_SHADER_WRITE_BIT,
+                        queueFamily = queueFamily,
+                        storeResult = prefilledStorageResult
+                    ) { byteBuffer ->
+                        println("Prefilled storage buffer starts at ${memAddress(byteBuffer)}")
+                        check(byteBuffer.capacity() == 300)
+                    }
+                )
+
+                val prefilledVertexResult = CompletableDeferred<VulkanBufferRange>()
+                agent.claims.buffers.add(
+                    BufferMemoryClaim(
+                        size = 150,
+                        usageFlags = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+                        dstAccessMask = VK_ACCESS_VERTEX_ATTRIBUTE_READ_BIT,
+                        queueFamily = queueFamily,
+                        storeResult = prefilledVertexResult
+                    ) { byteBuffer ->
+                        println("Prefilled vertex buffer starts at ${memAddress(byteBuffer)}")
+                        check(byteBuffer.capacity() == 150)
+                    }
+                )
+
+                val uninitializedUniformResult = CompletableDeferred<VulkanBufferRange>()
+                agent.claims.buffers.add(
+                    BufferMemoryClaim(
+                        size = 400,
+                        usageFlags = VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
+                        queueFamily = queueFamily,
+                        storeResult = uninitializedUniformResult,
+                        prefill = null
+                    )
+                )
+
+                val uninitializedStorageResult = CompletableDeferred<VulkanBufferRange>()
+                agent.claims.buffers.add(
+                    BufferMemoryClaim(
+                        size = 345,
+                        usageFlags = VK_BUFFER_USAGE_STORAGE_BUFFER_BIT,
+                        queueFamily = queueFamily,
+                        storeResult = uninitializedStorageResult,
+                        prefill = null
+                    )
+                )
+
+                val prefilledColorResult = CompletableDeferred<VulkanImage>()
+                agent.claims.images.add(
+                    ImageMemoryClaim(
+                        width = 50, height = 70, queueFamily = queueFamily,
+                        bytesPerPixel = 4, imageFormat = VK_FORMAT_R8G8B8A8_SINT,
+                        tiling = VK_IMAGE_TILING_OPTIMAL, samples = VK_SAMPLE_COUNT_4_BIT,
+                        imageUsage = VK_IMAGE_USAGE_INPUT_ATTACHMENT_BIT, initialLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
+                        accessMask = VK_ACCESS_SHADER_READ_BIT, aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+                        storeResult = prefilledColorResult
+                    ) { byteBuffer ->
+                        println("Prefilled color staging buffer starts at ${memAddress(byteBuffer)}")
+                        check(byteBuffer.capacity() == 50 * 70 * 4)
+                    }
+                )
+
+                val prefilledDepthResult = CompletableDeferred<VulkanImage>()
+                agent.claims.images.add(
+                    ImageMemoryClaim(
+                        width = 60, height = 20, queueFamily = queueFamily,
+                        bytesPerPixel = 4, imageFormat = VK_FORMAT_D32_SFLOAT,
+                        tiling = VK_IMAGE_TILING_OPTIMAL, samples = VK_SAMPLE_COUNT_1_BIT,
+                        imageUsage = VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT, initialLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL,
+                        accessMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT, aspectMask = VK_IMAGE_ASPECT_DEPTH_BIT,
+                        storeResult = prefilledDepthResult
+                    ) { byteBuffer ->
+                        println("Prefilled depth staging buffer starts at ${memAddress(byteBuffer)}")
+                        check(byteBuffer.capacity() == 60 * 20 * 4)
+                    }
+                )
+
+                val uninitializedColorResult = CompletableDeferred<VulkanImage>()
+                agent.claims.images.add(
+                    ImageMemoryClaim(
+                        width = 10, height = 11, queueFamily = queueFamily,
+                        bytesPerPixel = 4, imageFormat = VK_FORMAT_R8G8B8A8_SRGB,
+                        tiling = VK_IMAGE_TILING_OPTIMAL, samples = VK_SAMPLE_COUNT_1_BIT,
+                        imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT, initialLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
+                        accessMask = VK_ACCESS_SHADER_READ_BIT, aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+                        storeResult = uninitializedColorResult, prefill = null
+                    )
+                )
+
+                val uninitializedDepthResult = CompletableDeferred<VulkanImage>()
+                agent.claims.images.add(
+                    ImageMemoryClaim(
+                        width = 100, height = 70, queueFamily = queueFamily,
+                        bytesPerPixel = 4, imageFormat = VK_FORMAT_D32_SFLOAT,
+                        tiling = VK_IMAGE_TILING_OPTIMAL, samples = VK_SAMPLE_COUNT_2_BIT,
+                        imageUsage = VK_IMAGE_USAGE_INPUT_ATTACHMENT_BIT, initialLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
+                        accessMask = VK_ACCESS_SHADER_READ_BIT, aspectMask = VK_IMAGE_ASPECT_DEPTH_BIT,
+                        storeResult = uninitializedDepthResult, prefill = null
+                    )
+                )
+
+                val indexStagingResult = CompletableDeferred<Pair<ByteBuffer, VulkanBufferRange>>()
+                agent.claims.stagingBuffers.add(
+                    StagingBufferMemoryClaim(
+                        size = 543, queueFamily = queueFamily, storeResult = indexStagingResult
+                    )
+                )
+
+                agent.gameInitScope.launch {
+                    val prefilledIndirectBuffer = prefilledIndirectResult.await()
+                    val prefilledStorageBuffer = prefilledStorageResult.await()
+                    val prefilledVertexBuffer = prefilledVertexResult.await()
+                    val uninitializedUniformBuffer = uninitializedUniformResult.await()
+                    val uninitializedStorageBuffer = uninitializedStorageResult.await()
+                    val prefilledColorImage = prefilledColorResult.await()
+                    val prefilledDepthImage = prefilledDepthResult.await()
+                    val uninitializedColorImage = uninitializedColorResult.await()
+                    val uninitializedDepthImage = uninitializedDepthResult.await()
+                    val indexStagingBuffer = indexStagingResult.await()
+
+                    println("Prefilled indirect buffer is $prefilledIndirectBuffer")
+                    println("Prefilled storage buffer is $prefilledStorageBuffer")
+                    println("Prefilled vertex buffer is $prefilledVertexBuffer")
+                    println("Uninitialized uniform buffer is $uninitializedUniformBuffer")
+                    println("Uninitialized storage buffer is $uninitializedStorageBuffer")
+                    println("Prefilled color image is $prefilledColorImage")
+                    println("Prefilled depth image is $prefilledDepthImage")
+                    println("Uninitialized color image is $uninitializedColorImage")
+                    println("Uninitialized depth image is $uninitializedDepthImage")
+                    println("Index staging buffer is ${indexStagingBuffer.second} at CPU address ${memAddress(indexStagingBuffer.first)}")
+
+                    check(prefilledIndirectBuffer.size == 200L)
+                    check(prefilledStorageBuffer.size == 300L)
+                    check(prefilledVertexBuffer.size == 150L)
+                    check(uninitializedUniformBuffer.size == 400L)
+                    check(uninitializedStorageBuffer.size == 345L)
+                    check(indexStagingBuffer.first.capacity() == 543)
+                }
             }
         }
     }
