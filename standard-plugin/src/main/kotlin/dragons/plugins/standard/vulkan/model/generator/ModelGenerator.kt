279c385faaf111b27384acecd45d3a917f9a7346
knokko <knokogator@hotmail.com>
2021-12-05 17:30:55 +0100

    Write ModelGenerator.combine() and test it

--- standard-plugin/src/main/kotlin/dragons/plugins/standard/vulkan/model/generator/ModelGenerator.kt
+++ standard-plugin/src/main/kotlin/dragons/plugins/standard/vulkan/model/generator/ModelGenerator.kt
@@ -9,4 +9,42 @@ class ModelGenerator(
     val fillVertexBuffer: (Array<BasicVertex>) -> Unit,
     val fillIndexBuffer: (IntBuffer) -> Unit
 ) {
+    companion object {
+        fun combine(models: Collection<ModelGenerator>): ModelGenerator {
+            val numVertices = models.sumOf { it.numVertices }
+            val numIndices = models.sumOf { it.numIndices }
+
+            val modelList = models.toList()
+            val fillVertexBuffer = { vertexBuffer: Array<BasicVertex> ->
+                var nextVertexIndex = 0
+                for (model in modelList) {
+                    model.fillVertexBuffer(vertexBuffer.sliceArray(nextVertexIndex until nextVertexIndex + model.numVertices))
+                    nextVertexIndex += model.numVertices
+                }
+            }
+
+            val fillIndexBuffer = { indexBuffer: IntBuffer ->
+                var vertexOffset = 0
+                var nextIndexIndex = 0 // I kinda dislike this variable name
+                for (model in modelList) {
+                    model.fillIndexBuffer(indexBuffer.slice(nextIndexIndex, model.numIndices))
+
+                    // Increase all indices written by model.fillIndexBuffer to ensure they match the right vertex
+                    for (indexToIncrease in nextIndexIndex until nextIndexIndex + model.numIndices) {
+                        indexBuffer.put(indexToIncrease, indexBuffer[indexToIncrease] + vertexOffset)
+                    }
+
+                    vertexOffset += model.numVertices
+                    nextIndexIndex += model.numIndices
+                }
+            }
+
+            return ModelGenerator(
+                numVertices = numVertices,
+                numIndices = numIndices,
+                fillVertexBuffer = fillVertexBuffer,
+                fillIndexBuffer = fillIndexBuffer
+            )
+        }
+    }
 }
---
+++ standard-plugin/src/test/kotlin/dragons/plugins/standard/vulkan/model/generator/TestModelGenerator.kt
@@ -0,0 +1,75 @@
+package dragons.plugins.standard.vulkan.model.generator
+
+import dragons.plugins.standard.vulkan.vertex.BasicVertex
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.lwjgl.system.MemoryStack.stackPush
+
+class TestModelGenerator {
+
+    @Test
+    fun testCombine() {
+        val generator1 = ModelGenerator(
+            numVertices = 4,
+            numIndices = 6,
+            fillVertexBuffer = { vertexBuffer ->
+                vertexBuffer[0].matrixIndex = 1
+                vertexBuffer[1].matrixIndex = 2
+                vertexBuffer[2].matrixIndex = 3
+                vertexBuffer[3].matrixIndex = 4
+            },
+            fillIndexBuffer = { indexBuffer ->
+                indexBuffer.put(0)
+                indexBuffer.put(1)
+                indexBuffer.put(2)
+                indexBuffer.put(2)
+                indexBuffer.put(3)
+                indexBuffer.put(0)
+            }
+        )
+
+        val generator2 = ModelGenerator(
+            numVertices = 3,
+            numIndices = 3,
+            fillVertexBuffer = { vertexBuffer ->
+                vertexBuffer[0].matrixIndex = 5
+                vertexBuffer[1].matrixIndex = 6
+                vertexBuffer[2].matrixIndex = 7
+            },
+            fillIndexBuffer = { indexBuffer ->
+                indexBuffer.put(1)
+                indexBuffer.put(2)
+                indexBuffer.put(0)
+            }
+        )
+
+        val combinedGenerator = ModelGenerator.combine(listOf(generator1, generator2, generator1, generator2))
+        assertEquals(14, combinedGenerator.numVertices)
+        assertEquals(18, combinedGenerator.numIndices)
+
+        stackPush().use { stack ->
+            val vertexBuffer = BasicVertex.createArray(stack.calloc(14 * BasicVertex.SIZE), 0, 14)
+            combinedGenerator.fillVertexBuffer(vertexBuffer)
+
+            var expectedMatrixIndices = arrayOf(
+                1, 2, 3, 4, 5, 6, 7
+            )
+            expectedMatrixIndices += expectedMatrixIndices
+            for ((index, expectedMatrixIndex) in expectedMatrixIndices.withIndex()) {
+                assertEquals(expectedMatrixIndex, vertexBuffer[index].matrixIndex)
+            }
+
+            val indexBuffer = stack.callocInt(18)
+            combinedGenerator.fillIndexBuffer(indexBuffer)
+            val expectedIndices = arrayOf(
+                0, 1, 2, 2, 3, 0,
+                5, 6, 4,
+                7, 8, 9, 9, 10, 7,
+                12, 13, 11
+            )
+            for (expectedIndex in expectedIndices) {
+                assertEquals(expectedIndex, indexBuffer.get())
+            }
+        }
+    }
+}
