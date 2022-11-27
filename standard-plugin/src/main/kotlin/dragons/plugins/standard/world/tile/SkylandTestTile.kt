420f73aa03122fcf1c1fa492d96a6e4c222d28c6
knokko <knokogator@hotmail.com>
2022-11-27 09:38:37 +0100

    debugging...

--- standard-plugin/src/main/kotlin/dragons/plugins/standard/world/tile/SkylandTestTile.kt
+++ standard-plugin/src/main/kotlin/dragons/plugins/standard/world/tile/SkylandTestTile.kt
@@ -1,6 +1,9 @@
 package dragons.plugins.standard.world.tile
 
+import dragons.plugins.standard.vulkan.model.generator.dragon.DragonWingProperties
+import dragons.plugins.standard.vulkan.model.generator.dragon.createDragonWingGenerator
 import dragons.plugins.standard.vulkan.model.generator.generateSkylandModel
+import dragons.plugins.standard.vulkan.model.matrices.createDragonWingMatrices
 import dragons.plugins.standard.vulkan.render.StandardSceneRenderer
 import dragons.plugins.standard.vulkan.render.tile.TileMemoryClaimAgent
 import dragons.plugins.standard.vulkan.render.tile.TileRenderer
@@ -9,6 +12,7 @@ import dragons.plugins.standard.vulkan.render.tile.TileRendererFactory
 import dragons.plugins.standard.vulkan.util.claimColorImage
 import dragons.plugins.standard.vulkan.util.claimHeightImage
 import dragons.plugins.standard.vulkan.util.claimVertexAndIndexBuffer
+import dragons.space.Distance
 import dragons.space.Position
 import dragons.vulkan.memory.VulkanBufferRange
 import dragons.vulkan.memory.VulkanImage
@@ -17,6 +21,16 @@ import dragons.world.tile.TileProperties
 import dragons.world.tile.TileState
 import kotlinx.coroutines.CompletableDeferred
 import org.joml.Matrix4f
+import org.joml.Random
+
+val props = DragonWingProperties(
+    baseWingLength = Distance.meters(0.5f),
+    wingLaneWidth = Distance.meters(0.2f),
+    wingTopLength = Distance.meters(0.3f),
+    wingDepth = Distance.milliMeters(30),
+    nailLength = Distance.meters(0.3f),
+    nailWidth = Distance.meters(0.15f)
+)
 
 class SkylandTestTile(position: Position): TileProperties(position) {
 
@@ -32,10 +46,14 @@ class SkylandTestTile(position: Position): TileProperties(position) {
 
         override fun render(renderer: StandardSceneRenderer, tile: SmallTile, cameraPosition: Position) {
             val renderPosition = tile.properties.position - cameraPosition
+            val rng = Random()
             val transformationMatrix = Matrix4f()
                 .translate(renderPosition.x.meters, renderPosition.y.meters, renderPosition.z.meters)
-                .scale(10f)
-            renderer.drawTile(vertices, indices, arrayOf(transformationMatrix))
+                .rotateX(6f * rng.nextFloat())
+                .rotateY(6f * rng.nextFloat())
+                .rotateZ(6f * rng.nextFloat())
+                .scale(100f)
+            renderer.drawTile(vertices, indices, createDragonWingMatrices(transformationMatrix, props))
         }
 
         class Factory: TileRendererFactory<SkylandTestTile> {
@@ -54,8 +72,16 @@ class SkylandTestTile(position: Position): TileProperties(position) {
                 val heightTexture = CompletableDeferred<VulkanImage>()
 
                 val graphics = agent.gameState.graphics
+//                claimVertexAndIndexBuffer(
+//                    agent.claims, graphics.queueManager, vertices, indices, generateSkylandModel { 0.5f },
+//                    listOf(agent.claimColorImageIndex(colorTexture)), listOf(agent.claimHeightImageIndex(heightTexture)),
+//                    "standard plug-in:SkylandTestTile"
+//                )
+
                 claimVertexAndIndexBuffer(
-                    agent.claims, graphics.queueManager, vertices, indices, generateSkylandModel { 0.5f },
+                    agent.claims, graphics.queueManager, vertices, indices, createDragonWingGenerator(
+                        props, listOf(0, 1, 2, 3, 4), false
+                    ),
                     listOf(agent.claimColorImageIndex(colorTexture)), listOf(agent.claimHeightImageIndex(heightTexture)),
                     "standard plug-in:SkylandTestTile"
                 )
@@ -63,11 +89,15 @@ class SkylandTestTile(position: Position): TileProperties(position) {
                     agent.claims, graphics.queueManager, agent.gameState.classLoader, 1024, 1024, colorTexture,
                     "dragons/plugins/standard/images/testTerrain.jpg", "standard plug-in:SkylandTestTile"
                 )
+//                claimHeightImage(
+//                    agent.claims, graphics.queueManager, agent.gameState.classLoader, 128, 128, heightTexture,
+//                    "dragons/plugins/standard/images/testTerrainHeight.png", 0.001f,
+//                    "standard plug-in:SkylandTestTile"
+//                )
                 claimHeightImage(
-                    agent.claims, graphics.queueManager, agent.gameState.classLoader, 128, 128, heightTexture,
-                    "dragons/plugins/standard/images/testTerrainHeight.png", 0.001f,
+                    agent.claims, graphics.queueManager, 128, 128, heightTexture,
                     "standard plug-in:SkylandTestTile"
-                )
+                ) { _, _ -> 0f }
             }
 
             override suspend fun createRenderer() = Renderer(
