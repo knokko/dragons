1292d04570ca96ba01cbd85db2e89153d2fb0370
knokko <knokogator@hotmail.com>
2023-01-12 11:57:40 +0100

    Add support for non-unit directions to LineCircleIntersection

--- src/main/kotlin/dragons/space/shape/LineCircleIntersection.kt
+++ src/main/kotlin/dragons/space/shape/LineCircleIntersection.kt
@@ -5,33 +5,44 @@ import org.joml.Math.sqrt
 import org.joml.Vector2f
 
 fun determineLineCircleIntersections(
-    centerX: Distance, centerY: Distance, radius: Distance, lineX: Distance, lineY: Distance, unitDirection: Vector2f
+    centerX: Distance, centerY: Distance, radius: Distance, lineX: Distance, lineY: Distance, direction: Vector2f
 ): Pair<Distance, Distance>? {
-    val perpendicularDirection = Vector2f(unitDirection).perpendicular()
+
+    // If direction ~= 0, we simply check whether the line start is inside the circle
+    if (direction.length() < 0.0001f) {
+        val dx = (centerX - lineX).meters
+        val dy = (centerY - lineY).meters
+        return if (dx * dx + dy * dy <= radius.meters * radius.meters) {
+            // Returning (0, 100k) is kinda dirty, but useful in most cases
+            Pair(Distance.meters(0), Distance.kiloMeters(100))
+        } else null
+    }
+
+    val perpendicularDirection = Vector2f(direction).perpendicular()
 
     /*
      * First, we need to find the closest point on the line to the center of the circle.
      *
      * Mathematics:
-     * ud = unitDirection, pd = perpendicularDirection, c = center, l = lineStart, r = radius,
-     * a = distanceFromCenterToClosestPoint, b = distanceFromLineStartToClosestPoint
+     * ld = (line)direction, pd = perpendicularDirection, c = center, l = lineStart, r = radius,
+     * a * |pd| = distanceFromCenterToClosestPoint, b * |ld| = distanceFromLineStartToClosestPoint
      *
-     * Closest point = c + a * pd = l + b * ud | with a and b unknown
-     * (1) c.x + a * pd.x = l.x + b * ud.x
-     *     a * pd.x = l.x - c.x + b * ud.x
-     *     a = (l.x - c.x + b * ud.x) / pd.x = (l.y - c.y + b * ud.y) / pd.y
+     * Closest point = c + a * pd = l + b * ld | with a and b unknown
+     * (1) c.x + a * pd.x = l.x + b * ld.x
+     *     a * pd.x = l.x - c.x + b * ld.x
+     *     a = (l.x - c.x + b * ld.x) / pd.x = (l.y - c.y + b * ld.y) / pd.y
      *
-     * (2) (l.x - c.x + b * ud.x) / pd.x = (l.y - c.y + b * ud.y) / pd.y
-     *     (l.x - c.x + b * ud.x) * pd.y = (l.y - c.y + b * ud.y) * pd.x
-     *     b * ud.x * pd.y + (l.x - c.x) * pd.y = b * ud.y * pd.x + (l.y - c.y) * pd.x
-     *     b * (ud.x * pd.y - ud.y * pd.x) = (l.y - c.y) * pd.x - (l.x - c.x) * pd.y
-     *     b = ((l.y - c.y) * pd.x - (l.x - c.x) * pd.y)) / (ud.x * pd.y - ud.y * pd.x)
+     * (2) (l.x - c.x + b * ld.x) / pd.x = (l.y - c.y + b * ld.y) / pd.y
+     *     (l.x - c.x + b * ld.x) * pd.y = (l.y - c.y + b * ld.y) * pd.x
+     *     b * ld.x * pd.y + (l.x - c.x) * pd.y = b * ld.y * pd.x + (l.y - c.y) * pd.x
+     *     b * (ld.x * pd.y - ld.y * pd.x) = (l.y - c.y) * pd.x - (l.x - c.x) * pd.y
+     *     b = ((l.y - c.y) * pd.x - (l.x - c.x) * pd.y)) / (ld.x * pd.y - ld.y * pd.x)
      */
-    val distanceFromLineStartToClosestPoint =
-        ((lineY - centerY) * perpendicularDirection.x - (lineX - centerX) * perpendicularDirection.y) /
-            (unitDirection.x * perpendicularDirection.y - unitDirection.y * perpendicularDirection.x)
-    val closestPointX = lineX + distanceFromLineStartToClosestPoint * unitDirection.x
-    val closestPointY = lineY + distanceFromLineStartToClosestPoint * unitDirection.y
+    val b = ((lineY - centerY) * perpendicularDirection.x - (lineX - centerX) * perpendicularDirection.y) /
+            (direction.x * perpendicularDirection.y - direction.y * perpendicularDirection.x)
+    val distanceFromLineStartToClosestPoint = b * direction.length()
+    val closestPointX = lineX + b * direction.x
+    val closestPointY = lineY + b * direction.y
 
     val distanceFromCenterToClosestPointSq = run {
         val dx = (centerX - closestPointX).meters
@@ -48,7 +59,7 @@ fun determineLineCircleIntersections(
      * c = distanceFromCircleOutlineToClosestPoint
      *
      * In the triangle (lineStart, center, closestPoint), the angle at closestPoint is 90 degrees since
-     * pd is orthogonal to ud (by definition). Since any intersection point must be located on the line through
+     * pd is orthogonal to ld (by definition). Since any intersection point must be located on the line through
      * lineStart and closestPoint, the angle at closestPoint in any triangle (closestPoint, center, intersectionPoint)
      * must also be 90 degrees. This makes it possible to use Pythagoras to compute the distance from the closest
      * point to the intersection points: a^2 + c^2 = r^2 -> c^2 = r^2 - a^2
@@ -66,7 +77,7 @@ fun determineLineCircleIntersections(
 
     val distanceFromCircleOutlineToClosestPoint = Distance.meters(sqrt(distanceFromCircleOutlineToClosestPointSq))
     return Pair(
-        distanceFromLineStartToClosestPoint - distanceFromCircleOutlineToClosestPoint,
-        distanceFromLineStartToClosestPoint + distanceFromCircleOutlineToClosestPoint
+        (distanceFromLineStartToClosestPoint - distanceFromCircleOutlineToClosestPoint) / direction.length(),
+        (distanceFromLineStartToClosestPoint + distanceFromCircleOutlineToClosestPoint) / direction.length()
     )
 }
--- src/test/kotlin/dragons/space/shape/TestLineCircleIntersection.kt
+++ src/test/kotlin/dragons/space/shape/TestLineCircleIntersection.kt
@@ -14,38 +14,38 @@ class TestLineCircleIntersection {
         // Horizontal line below the circle
         assertNull(determineLineCircleIntersections(
             centerX = Distance.meters(50), centerY = Distance.meters(100), radius = Distance.meters(20),
-            lineX = Distance.meters(10), lineY = Distance.meters(40), unitDirection = Vector2f(1f, 0f)
+            lineX = Distance.meters(10), lineY = Distance.meters(40), direction = Vector2f(1f, 0f)
         ))
 
         // Horizontal line above the circle
         assertNull(determineLineCircleIntersections(
             centerX = Distance.meters(50), centerY = Distance.meters(100), radius = Distance.meters(20),
-            lineX = Distance.meters(10), lineY = Distance.meters(140), unitDirection = Vector2f(1f, 0f)
+            lineX = Distance.meters(10), lineY = Distance.meters(140), direction = Vector2f(1f, 0f)
         ))
 
         // Vertical line on the left of the circle
         assertNull(determineLineCircleIntersections(
             centerX = Distance.meters(50), centerY = Distance.meters(100), radius = Distance.meters(20),
-            lineX = Distance.meters(10), lineY = Distance.meters(40), unitDirection = Vector2f(0f, 1f)
+            lineX = Distance.meters(10), lineY = Distance.meters(40), direction = Vector2f(0f, 1f)
         ))
 
         // Vertical line on the right of the circle
         assertNull(determineLineCircleIntersections(
             centerX = Distance.meters(50), centerY = Distance.meters(100), radius = Distance.meters(20),
-            lineX = Distance.meters(100), lineY = Distance.meters(40), unitDirection = Vector2f(0f, 1f)
+            lineX = Distance.meters(100), lineY = Distance.meters(40), direction = Vector2f(0f, 1f)
         ))
 
         // Diagonal line below the circle
         assertNull(determineLineCircleIntersections(
             centerX = Distance.meters(50), centerY = Distance.meters(100), radius = Distance.meters(20),
-            lineX = Distance.meters(10), lineY = Distance.meters(10), unitDirection = Vector2f(0.7f, 0.7f)
+            lineX = Distance.meters(10), lineY = Distance.meters(10), direction = Vector2f(0.7f, 0.7f)
         ))
 
         // Diagonal line that approaches the circle very closely
         assertNull(determineLineCircleIntersections(
             centerX = Distance.meters(0), centerY = Distance.meters(0), radius = Distance.meters(10),
             lineX = Distance.meters(0), lineY = Distance.meters(-14.5f),
-            unitDirection = Vector2f(0.5f * sqrt(2f))
+            direction = Vector2f(0.5f * sqrt(2f))
         ))
     }
 
@@ -57,21 +57,27 @@ class TestLineCircleIntersection {
     @Test
     fun testDetermineLineCircleIntersectionHorizontalHit() {
         // Horizontal line through the lower half of the circle
-        assertHit(40f - sqrt(300f), 40f + sqrt(300f), determineLineCircleIntersections(
+        assertHit(20f - 5f * sqrt(3f), 20f + 5f * sqrt(3f), determineLineCircleIntersections(
             centerX = Distance.meters(50), centerY = Distance.meters(100), radius = Distance.meters(20),
-            lineX = Distance.meters(10), lineY = Distance.meters(90), unitDirection = Vector2f(1f, 0f)
+            lineX = Distance.meters(10), lineY = Distance.meters(90), direction = Vector2f(2f, 0f)
         ))
 
         // Horizontal right-to-left line straight through the center of the circle
         assertHit(80f, 120f, determineLineCircleIntersections(
             centerX = Distance.meters(50), centerY = Distance.meters(100), radius = Distance.meters(20),
-            lineX = Distance.meters(150), lineY = Distance.meters(100), unitDirection = Vector2f(-1f, 0f)
+            lineX = Distance.meters(150), lineY = Distance.meters(100), direction = Vector2f(-1f, 0f)
         ))
 
         // Horizontal line through the upper half of the circle
         assertHit(40f - sqrt(300f), 40f + sqrt(300f), determineLineCircleIntersections(
             centerX = Distance.meters(50), centerY = Distance.meters(100), radius = Distance.meters(20),
-            lineX = Distance.meters(10), lineY = Distance.meters(110), unitDirection = Vector2f(1f, 0f)
+            lineX = Distance.meters(10), lineY = Distance.meters(110), direction = Vector2f(1f, 0f)
+        ))
+
+        // Horizontal line in the wrong direction
+        assertHit(-60f, -40f, determineLineCircleIntersections(
+            centerX = Distance.meters(50), centerY = Distance.meters(50), radius = Distance.meters(10),
+            lineX = Distance.meters(100), lineY = Distance.meters(50), direction = Vector2f(1f, 0f)
         ))
     }
 
@@ -80,19 +86,19 @@ class TestLineCircleIntersection {
         // Vertical line that intersects at the left of the center
         assertHit(60f - sqrt(300f), 60f + sqrt(300f), determineLineCircleIntersections(
             centerX = Distance.meters(50), centerY = Distance.meters(100), radius = Distance.meters(20),
-            lineX = Distance.meters(40), lineY = Distance.meters(40), unitDirection = Vector2f(0f, 1f)
+            lineX = Distance.meters(40), lineY = Distance.meters(40), direction = Vector2f(0f, 1f)
         ))
 
         // Vertical down-going line that goes straight through the center
-        assertHit(40f, 80f, determineLineCircleIntersections(
+        assertHit(80f, 160f, determineLineCircleIntersections(
             centerX = Distance.meters(50), centerY = Distance.meters(100), radius = Distance.meters(20),
-            lineX = Distance.meters(50), lineY = Distance.meters(160), unitDirection = Vector2f(0f, -1f)
+            lineX = Distance.meters(50), lineY = Distance.meters(160), direction = Vector2f(0f, -0.5f)
         ))
 
         // Vertical line that intersects at the right of the center
         assertHit(60f - sqrt(300f), 60f + sqrt(300f), determineLineCircleIntersections(
             centerX = Distance.meters(50), centerY = Distance.meters(100), radius = Distance.meters(20),
-            lineX = Distance.meters(60), lineY = Distance.meters(40), unitDirection = Vector2f(0f, 1f)
+            lineX = Distance.meters(60), lineY = Distance.meters(40), direction = Vector2f(0f, 1f)
         ))
     }
 
@@ -102,21 +108,36 @@ class TestLineCircleIntersection {
         assertHit(sqrt(5000f), sqrt(7200f), determineLineCircleIntersections(
             centerX = Distance.meters(100), centerY = Distance.meters(100), radius = Distance.meters(10),
             lineX = Distance.meters(50), lineY = Distance.meters(160),
-            unitDirection = Vector2f(0.5f * sqrt(2f), -0.5f * sqrt(2f))
+            direction = Vector2f(0.5f * sqrt(2f), -0.5f * sqrt(2f))
         ))
 
         // Right-up diagonal line that goes through the center of the circle
-        assertHit(sqrt(20_000f) - 10f, sqrt(20_000f) + 10f, determineLineCircleIntersections(
+        assertHit((sqrt(20_000f) - 10f) / 3f, (sqrt(20_000f) + 10f) / 3f, determineLineCircleIntersections(
             centerX = Distance.meters(100), centerY = Distance.meters(0), radius = Distance.meters(10),
             lineX = Distance.meters(0), lineY = Distance.meters(-100),
-            unitDirection = Vector2f(0.5f * sqrt(2f))
+            direction = Vector2f(1.5f * sqrt(2f))
         ))
 
         // Left-up diagonal line that intersects the circle at 270 degrees and 180 degrees
         assertHit(sqrt(20_000f), sqrt(24_200f), determineLineCircleIntersections(
             centerX = Distance.meters(0), centerY = Distance.meters(0), radius = Distance.meters(10),
             lineX = Distance.meters(100), lineY = Distance.meters(-110f),
-            unitDirection = Vector2f(-0.5f * sqrt(2f), 0.5f * sqrt(2f))
+            direction = Vector2f(-0.5f * sqrt(2f), 0.5f * sqrt(2f))
+        ))
+    }
+
+    @Test
+    fun testDetermineLineCircleIntersectionZeroDirection() {
+        // Zero-direction line that starts inside the circle
+        assertHit(0f, 100_000f, determineLineCircleIntersections(
+            centerX = -Distance.meters(50), centerY = -Distance.meters(100), radius = Distance.meters(5),
+            lineX = -Distance.meters(51), lineY = -Distance.meters(100), direction = Vector2f()
+        ))
+
+        // Zero-direction line that starts outside the circle
+        assertNull(determineLineCircleIntersections(
+            centerX = -Distance.meters(40), centerY = -Distance.meters(100), radius = Distance.meters(5),
+            lineX = -Distance.meters(51), lineY = -Distance.meters(100), direction = Vector2f()
         ))
     }
 }
