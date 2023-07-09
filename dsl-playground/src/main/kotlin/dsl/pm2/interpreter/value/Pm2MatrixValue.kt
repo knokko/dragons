781480527649ff3403a37988ce7f17988b66d82d
knokko <knokogator@hotmail.com>
2023-07-09 12:21:09 +0200

    PM2: Implement more toString()s

--- dsl-playground/src/main/kotlin/dsl/pm2/interpreter/value/Pm2MatrixValue.kt
+++ dsl-playground/src/main/kotlin/dsl/pm2/interpreter/value/Pm2MatrixValue.kt
@@ -4,4 +4,6 @@ import org.joml.Matrix3x2f
 
 class Pm2MatrixValue(internal val matrix: Matrix3x2f): Pm2Value() {
     override fun copy() = Pm2MatrixValue(Matrix3x2f(matrix))
+
+    override fun toString() = "MutableMatrix($matrix)"
 }
